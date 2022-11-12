2aa184092a08e97efe6fd961a76a7ed5bccc18c1
Heorhii <georgii.ua@gmail.com>
2022-11-12 12:53:55 +0200

    try

--- app/js/main.js
+++ app/js/main.js
@@ -1,96 +1,25 @@
-var cardCount = 0;
-const urls = ['heli_1.jpg', 'heli_2.jpg','heli_3.jpeg','heli_4.jpg','heli_5.jpg',
-  'pistol_1.jpg', 'pistol_2.jpg','pistol_3.jpg','pistol_4.jpg','pistol_5.jpg',
-  'plane_1.jpg', 'plane_2.jpg','plane_3.jpg','plane_4.jpg','plane_5.jpg',
-  'rifle_1.jpg','rifle_2.jpg','rifle_3.jpg','rifle_4.jpg','rifle_5.jpg',
-  'tank_1.jpg', 'tank_2.jpg', 'tank_3.png', 'tank_4.jpg', 'tank_5.jpg'];
-
-$(document).ready(function(){
-  setImage();
-  $('.cell').droppable({
-    over: function (){
-      $(this).css("border", "solid yellow");
-    },
-    out: function (){
-      $(this).css("border", "none");
-    },
-    drop: function (){
-      $(this).css("border", "none");
-
-      compare($(this), $(".game__field__guess"));
-    }
+$(document).ready(function() {
+  $(".slider").each(function () {
+    var obj = $(this);
+    $(obj).append("<div class='nav'></div>");
+    $(obj).find("li").each(function () {
+      $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>");
+      $(this).addClass("slider"+$(this).index());
+    });
+    $(obj).find("span").first().addClass("on");
   });
-      $(".game__field__guess").draggable({
-        revert: true
-      });
-
-
-  });
-function startGame(){
-  $(".main-menu").css("display", "none");
-  $(".game").css("display", "flex");
-  var randomItem = getRandomInt(0,25);
-  $(".game__field__guess").css("background-image", "url(./img/"+urls[randomItem]+")");
-  $(".game__field__guess").data("data-file", urls[randomItem])
-  //$(".game__field__guess").addClass(urls[randomItem]);
+});
+function sliderJS (obj, sl) {
+  var ul = $(sl).find("ul"); // находим блок
+  var bl = $(sl).find("li.slider"+obj);
+  var step = $(bl).width(); // ширина объекта
+  $(ul).animate({marginLeft: "-"+step*obj}, 500);
 }
-function refresh(){
-  document.location.reload();
-}
-function showpopup(message, button)
-{
- $(".game__popup").css({"visibility":"visible","display":"flex"});
-  $(".game__popup__text").text(message);
-  $(".game__popup__ok").text(button);
-}
-function hidepopup()
-{
- $(".game__popup").css({"visibility":"hidden","display":"flex"});
-}
-function setImage(){
-  var arr = randomUniqueNum(25, 25);
-  var i = 0;
-  $('.cell').each(function (index,element){
-    $(element).css("background-image", "url(./img/"+urls[arr[i]]+")");
-    $(element).data("data-file", urls[arr[i]]);
-    i++;
-  });
-}
-function randomUniqueNum(range, outputCount) {
-    var arr = []
-    for (var i = 0; i <= range; i++) {
-      arr.push(i)
-    }
-    var result = [];
-    for (var i = 1; i <= outputCount; i++) {
-      const random = Math.floor(Math.random() * (range - i));
-      result.push(arr[random]);
-      arr[random] = arr[range - i];
-    }
-    return result;
-}
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
-}
-function compare(drop, drag){
-  var find = false;
-  console.log($(drop).data("data-file"))
-  if($(drop).data("data-file")===$(drag).data("data-file")){
-    find = true;
-  }
-  if(find){
-    var randomItem = getRandomInt(0,25);
-    $(".game__field__guess").css("background-image", "url(./img/"+urls[randomItem]+")");
-    $(".game__field__guess").data("data-file", urls[randomItem]);
-    var path = "./img/star.png"
-    $(".stars").append("<img src="+path+">");
-    cardCount++;
-    if(cardCount==10){
-      showpopup("You win!", "Retry");
-    }
-  }else{
-    showpopup("You loose!", "Retry");
-  }
-}
+$(document).on("click", ".slider .nav span", function() {
+  var sl = $(this).closest(".slider");
+  $(sl).find("span").removeClass("on");
+  $(this).addClass("on");
+  var obj = $(this).attr("rel");
+  sliderJS(obj, sl);
+  return false;
+});
